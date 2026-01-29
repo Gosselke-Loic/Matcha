53417f19b1239f3a919133835fbf77fd2240f73d
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2026-01-29 16:27:18 +0100

    feat(frontend): Update __root.tsx
    Adding fallback router.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,1 +1,21 @@
+import type { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet, Link } from "@tanstack/react-router";
 
+interface RouterContext {
+    queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+    component: () => (
+        <>
+            {/* Layout */}
+            <Outlet />
+        </>
+    ),
+    notFoundComponent: () => (
+        <div>
+            <p>Oups! Page not found</p>
+            <Link to="/">Return to homepage</Link>
+        </div>
+    )
+});
