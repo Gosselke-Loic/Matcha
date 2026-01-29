a57562db306b74284461fd0c00144d954a42871b
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2026-01-29 15:46:45 +0100

    feat(frontend): Update main.tsx
    Add tanstack router/query implementation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,32 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM  from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import './index.css'
-import App from './App.tsx'
+import './index.css';
 
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const router = createRouter({
+	context: {
+		queryClient
+	},
+	defaultPreload: "intent",
+	defaultPreloadStaleTime: 0,
+	scrollRestoration: true
+});
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+};
+
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
+};
