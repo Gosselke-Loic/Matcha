9946f86c4463f4343d6c764224afd033fb3bc493
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2026-01-29 16:18:16 +0100

    feat(frontend): Update main.tsx
    Add StrictMode to application.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import ReactDOM  from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -25,8 +26,10 @@ const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</StrictMode>
 	);
 };
