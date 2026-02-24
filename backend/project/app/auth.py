b4acf5dbdb2618e722c660d58a8fc5869d941313
sean <sbelomet@student.42lausanne.ch>
2026-02-24 12:15:31 +0100

    auth(backend): Testing

--- backend/project/app/auth.py
+++ backend/project/app/auth.py
@@ -92,3 +92,10 @@ def logout():
     resp = jsonify(msg="Logged out")
     unset_jwt_cookies(resp)
     return resp
+
+@bp.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
