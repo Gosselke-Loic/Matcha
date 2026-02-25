d1a7b20f0e3bc0366efdf3e9b36d12ce0fff4a2e
Typhon <gosselkeloic@gmail.com>
2026-02-25 12:40:28 +0100

    build(frontend): update build dependencies files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,14 +20,14 @@ export default defineConfig({
 		}
 	},
 	resolve: {
-	 alias: {
-	  '@': path.resolve(__dirname, './src'),
-          '@api': path.resolve(__dirname, './src/api'),
-	  '@hooks': path.resolve(__dirname, './src/hooks'),
-          '@assets': path.resolve(__dirname, './src/assets'),
-	  '@routes': path.resolve(__dirname, './src/routes'),
-          '@features': path.resolve(__dirname, './src/features'),
-	  '@components': path.resolve(__dirname, './src/components')
-	 }
-       }
+		alias: {
+		  '@': path.resolve(__dirname, './src'),
+		  '@api': path.resolve(__dirname, './src/api'),
+		  '@hooks': path.resolve(__dirname, './src/hooks'),
+		  '@assets': path.resolve(__dirname, './src/assets'),
+		  '@routes': path.resolve(__dirname, './src/routes'),
+			'@features': path.resolve(__dirname, './src/features'),
+		  '@components': path.resolve(__dirname, './src/components')
+		}
+  }
 })
