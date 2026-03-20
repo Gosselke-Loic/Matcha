77dcb1713911838ee27628cda2bbf3b3c40ac160
Typhon <gosselkeloic@gmail.com>
2026-03-20 20:15:34 +0100

    feat(frontend/navbar): create navbar component, in progress

--- frontend/src/shared/components/layouts/Navbar.tsx
+++ frontend/src/shared/components/layouts/Navbar.tsx
@@ -1,6 +1,16 @@
+import { Link } from "@tanstack/react-router"; 
+import { useQuery } from "@tanstack/react-query";
+
+import { authMeOptions } from "@/features/auth/services/auth-options"; 
+
 export default function Navbar() {
+  const { data: user, isLoading } = useQuery(authMeOptions);
+
+  if (isLoading) return (<></>); // To do, skeleton navbar for loading
+  
   return (
-    <>
-    </>
+    <nav className="bg-white border-b border-gray-200 px-4 py-3 flex items-center justify-between">
+      
+    </nav>
   );
 };
